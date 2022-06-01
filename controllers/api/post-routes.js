4bdacaf831f94d876325803d9aaf10c8db647ba8
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-06-01 14:14:38 -0700

    conditionally render login links with handlebars helpers

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,12 +87,15 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-    .then(dbPostData => res.json(dbPostData))
+
+  if(req.session) {
+     Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+    .then(updatedPostData => res.json(updatedPostData))
     .catch(err=>{
         console.log(err);
         res.status(500).json(err);
     })
+  }
 });
 
 router.put('/:id', (req, res) => {
