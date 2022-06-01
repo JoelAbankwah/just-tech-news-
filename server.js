ffee4bd18ed5ce2eb6ca5fdbfaccdc60fa628dec
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-06-01 12:18:56 -0700

    add login and logout features and session storage with cookies

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const path = require('path');
 
+const session = require('express-session');
 const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +28,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
